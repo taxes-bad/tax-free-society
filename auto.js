c3587c66baec2a0db19853000adce7b047782053
Chance <68149062+pyralspite-ch0nce@users.noreply.github.com>
2021-04-11 20:03:57 +0530

    Update auto.js

--- auto.js
+++ auto.js
@@ -18,28 +18,6 @@ function BuyAutoPrinter() {
 var autoPrinting = setInterval(function() {money = money + AutoPrinters, document.getElementById('TotalMoney').innerHTML = money} , 1000)
 //1000ms = 1 second
 
-//get the modal
-var modal = document.getElementById("myModal");
-
-//get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-//finding offline progress 
-
-//open the modal 
-function openModal() {
-  modal.style.display = "block";
-  clearInterval(autoPrinting)
-}
-
-//when the player clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-  setInterval(function() {money = money + AutoPrinters, document.getElementById('TotalMoney').innerHTML = money} , 1000)
-}
-
-openModal();
-
 document.getElementById('TotalMoney').innerHTML = money;
 document.getElementById('AutoPrinterCost').innerHTML = AutoPrinterCost;
 document.getElementById('TotalAutoPrinters').innerHTML = AutoPrinters;
