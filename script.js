c699213ac2389f6c2f3a7526c6ee2931e3b03678
Chance <68149062+pyralspite-ch0nce@users.noreply.github.com>
2021-04-11 20:04:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,7 @@ var playerData = {
   money: money,
   AutoPrinters: AutoPrinters,
   AutoPrinterCost: AutoPrinterCost,
-  lastTick: Date.now()
 }
-
-//offline progress, thanks YHVR (u/YhvrTheSecond)
-var offlineLoop = window.setInterval(function() {
-  diff = Date.now() - playerData.lastTick;
-  playerData.lastTick = Date.now();
-  playerData.money += playerData.goldPerClick * (diff / 1000) // divide diff by how often (ms) mainGameLoop is ran
-  document.getElementById("goldMined").innerHTML = playerData.money + " Gold Mined"
-}, 1000)
 
 //incrementing the money and illegal money.
 function getrich() {
