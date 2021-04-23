7c10195e6bebeaccc3bf6db3ade438ed336a33c5
Chance <68149062+pyralspite-ch0nce@users.noreply.github.com>
2021-04-23 12:25:28 +0530

    Update auto.js

--- auto.js
+++ auto.js
@@ -13,43 +13,12 @@ function BuyAutoPrinter() {
 var autoPrinting = setInterval(function() {money = money + AutoPrinters, document.getElementById('TotalMoney').innerHTML = money} , 1000)
 //1000ms = 1 second
 
-//get the modal
-var modal = document.getElementById("modal");
-
-//get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
 //finding offline progress 
 var playerData = {
   money: money,
   AutoPrinters: AutoPrinters,
   AutoPrinterCost: AutoPrinterCost,
-  lastTick: Date.now()
 }
-
-//offline progress, thanks YHVR (u/YhvrTheSecond)
-var mainGameLoop = window.setInterval(function() {
-  diff = Date.now() - playerData.lastTick;
-  playerData.lastTick = Date.now(); // Don't forget to update lastTick.
-  var offlineMoney = Math.round((playerData.AutoPrinters * (diff/1000))); // divide diff by how often (ms) mainGameLoop is ran
-  playerData.money += offlineMoney;
-  document.getElementById('modalText').innerHTML = offlineMoney;
-  document.getElementById('TotalMoney').innerHTML = money;
-}, 1000)
-
-//open the modal 
-function openModal() {
-  modal.style.display = "block";
-  clearInterval(autoPrinting)
-}
-
-//when the player clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-  setInterval(function() {money = money + AutoPrinters, document.getElementById('TotalMoney').innerHTML = money} , 1000)
-}
-
-openModal();
 
 document.getElementById('TotalMoney').innerHTML = money;
 document.getElementById('AutoPrinterCost').innerHTML = AutoPrinterCost;
